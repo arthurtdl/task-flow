073c0594ef9862a865126f0f1ae6991197a5fd6e
Arthur Torres <arthur.torresdl03@gmail.com>
2026-07-17 19:18:44 -0300

    chore(docker-config): Added .env.example and refined server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+
+  const enviroment = process.env.NODE_ENV || 'development';
+
+  if (enviroment === 'development') {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  } else {
+    console.log(`🚀 Server is running in production mode on port ${PORT}`);
+  }
 });
