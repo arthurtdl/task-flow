0775b2f543b91aac6734cfee2bf78c3eb4cd9f5b
Arthur Torres <arthur.torresdl03@gmail.com>
2026-07-18 12:56:35 -0300

    feat(prisma-model): Finished prisma.config.ts and the database model

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,4 +1,9 @@
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
 import { defineConfig } from 'prisma/config';
+
+const myEnv = dotenv.config();
+dotenvExpand.expand(myEnv);
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
