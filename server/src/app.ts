cb9228ae48316a348aa375d6edec74c64528d9ff
Arthur Torres <arthur.torresdl03@gmail.com>
2026-07-18 16:16:47 -0300

    feat(crud-architecture): Implemented basic middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,13 @@
+import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
 
 app.get('/health', (req, res) => {
     res.json({ status: 'OK', message: 'Task Flow API is running!'});
