2cdb0df0dd0cab92e1b85f4b1a4338c4ecbeefe4
Arthur Torres <arthur.torresdl03@gmail.com>
2026-07-18 20:35:02 -0300

    refactor(user-crud): Removed express-async-errors

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,4 +6,8 @@ const routes = Router();
 // All the routes listed here will be prefixed with /api
 routes.use("/users", userRouter);
 
+routes.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the TaskFlow API' });
+})
+
 export default routes;
