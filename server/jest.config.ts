8aca1819cc41f0266e27741a5d4dfebe1c2c8345
Arthur Torres <arthur.torresdl03@gmail.com>
2026-07-19 22:30:54 -0300

    refactor(jwt-auth): Refactored all tests to the new auth logic

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,9 +1,11 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    clearMocks: true,
-    testMatch: ['**/*.spec.ts', '**/*.test.ts'],
-    moduleNameMapper: {
-    '^@database$': '<rootDir>/src/database',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  clearMocks: true,
+  testMatch: ["**/*.spec.ts", "**/*.test.ts"],
+  moduleNameMapper: {
+    "^@database$": "<rootDir>/src/database",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
-}
+  setupFiles: ['dotenv/config'],
+};
