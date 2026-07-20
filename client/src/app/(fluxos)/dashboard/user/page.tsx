62caada2219cd8908b4ecc853cf80a81caa78cb2
Arthur Torres <arthur.torresdl03@gmail.com>
2026-07-20 14:51:53 -0300

    Update page.tsx

--- client/src/app/(fluxos)/dashboard/user/page.tsx
+++ client/src/app/(fluxos)/dashboard/user/page.tsx
@@ -1,7 +1,9 @@
+import { UserDashboard } from "@/components/dashboard/user-dashboard"
+
 export default function UserPage() {
     return(
         <div>
-            user
+            <UserDashboard></UserDashboard>
         </div>
     )
 }
