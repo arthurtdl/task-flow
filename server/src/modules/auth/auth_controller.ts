6dd20e7051ea2f7fff033f7a72859f90cf2d69f6
Arthur Torres <arthur.torresdl03@gmail.com>
2026-07-21 00:36:52 -0300

    Update auth_controller.ts

--- server/src/modules/auth/auth_controller.ts
+++ server/src/modules/auth/auth_controller.ts
@@ -11,7 +11,7 @@ class AuthController {
     res.cookie('refreshToken', result.refreshToken, {
       httpOnly: true,  
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict', 
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, 
     });
 
@@ -28,7 +28,7 @@ class AuthController {
     res.clearCookie('refreshToken', {
       httpOnly: true,  
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict', 
+      sameSite: 'none', 
     });
 
     res.status(200).json({
